c30eb7a2fb341a46cb8affe700d548f069f351ee
Adin Baskoro <adinbaskoro.p@gmail.com>
2018-07-25 20:07:53 +0700

    Change tabs to spaces

--- main.go
+++ main.go
@@ -1,71 +1,71 @@
 package main
 
 import (
-  "log"
-  "time"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"github.com/joho/godotenv"
+    "log"
+    "time"
+    "encoding/json"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+    "github.com/joho/godotenv"
 )
 
 var discordStatus chan string
 
 type discordStatusStruct struct {
-	Status string
+    Status string
 }
 
 func handleMainPage(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hi, I'm Weiss! What can I do for you?"))
+    w.Write([]byte("Hi, I'm Weiss! What can I do for you?"))
 }
 
 func updateDiscordStatus(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        panic(err)
+    }
 
-	var message discordStatusStruct
-	err = json.Unmarshal(body, &message)
-	if err != nil {
-		panic(err)
-	}
+    var message discordStatusStruct
+    err = json.Unmarshal(body, &message)
+    if err != nil {
+        panic(err)
+    }
 
-	discordStatus <- message.Status
-	w.Write([]byte(message.Status))
+    discordStatus <- message.Status
+    w.Write([]byte(message.Status))
 }
 
 func main() {
-	discordStatus = make(chan string)
+    discordStatus = make(chan string)
 
-	if os.Getenv("ENV") != "production" {
+    if os.Getenv("ENV") != "production" {
     err := godotenv.Load()
     if err != nil {
       log.Fatal(err)
     }
   }
 
-	port := os.Getenv("PORT")
-	http.HandleFunc("/", handleMainPage)
-	http.HandleFunc("/discordstatus", updateDiscordStatus)
+    port := os.Getenv("PORT")
+    http.HandleFunc("/", handleMainPage)
+    http.HandleFunc("/discordstatus", updateDiscordStatus)
 
-	httpClient := &http.Client {
-		Timeout: time.Second * 10,
-	}
+    httpClient := &http.Client {
+        Timeout: time.Second * 10,
+    }
 
-	go StartDiscordBot(discordStatus, httpClient)
+    go StartDiscordBot(discordStatus, httpClient)
 
-	go func() {
-		err := http.ListenAndServe(":"+port, nil)
-		if err != nil {
-			panic(err)
-		}
-	}()
+    go func() {
+        err := http.ListenAndServe(":"+port, nil)
+        if err != nil {
+            panic(err)
+        }
+    }()
 
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
 }

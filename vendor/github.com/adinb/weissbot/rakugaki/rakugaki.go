f5b107481c3b943f75616d8b93ed31dcb9f29790
adinb <adinbaskoro.p@gmail.com>
2019-04-28 23:34:34 +0700

    Update deps in vendor

---
+++ vendor/github.com/adinb/weissbot/rakugaki/rakugaki.go
@@ -0,0 +1,34 @@
+package rakugaki
+
+import (
+	"math/rand"
+
+	"github.com/adinb/weissbot/twitter"
+)
+
+// GetRakugaki returns a random tweet that contains either #rkgk, rkgk, or らくがき with more than 100 favorites
+func GetRakugaki(tweetSearchFunc func(query string) ([]twitter.Tweet, error)) (twitter.Tweet, error) {
+	const favoriteThreshold = 100
+	hashtagRkgkTweets, err := tweetSearchFunc("%23rkgk")
+	if err != nil {
+		return twitter.Tweet{}, err
+	}
+
+	rkgkTweets, err := tweetSearchFunc("rkgk")
+	if err != nil {
+		return twitter.Tweet{}, err
+	}
+
+	rkgkJPTweets, err := tweetSearchFunc("%e3%82%89%e3%81%8f%e3%81%8c%e3%81%8d")
+	if err != nil {
+		return twitter.Tweet{}, err
+	}
+
+	var combinedTweets []twitter.Tweet
+	combinedTweets = append(combinedTweets, hashtagRkgkTweets...)
+	combinedTweets = append(combinedTweets, rkgkTweets...)
+	combinedTweets = append(combinedTweets, rkgkJPTweets...)
+	topTweets := twitter.FindTopTweets(favoriteThreshold, combinedTweets)
+
+	return topTweets[rand.Intn(len(topTweets))], nil
+}
